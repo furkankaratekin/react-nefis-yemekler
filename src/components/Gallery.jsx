87e6f7a8a1805ddeea8f1c5af7d1ada3a3d47abe
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-31 13:03:13 +0300

    Ana syfa Yönlendirme işlemi tamamlandı

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -26,22 +26,24 @@ const Gallery = ({ searchQuery }) => {
         <div className="flex flex-wrap -m-4">
           {filteredRecipes.map((recipe) => (
             <div className="lg:w-1/4 sm:w-1/2 p-4" key={recipe._id}>
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={recipe.picture}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    {recipe.category}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    {recipe.name}
-                  </h1>
-                  <p className="leading-relaxed">{recipe.recipe}</p>
+              <Link to={`/list-recipe/${recipe._id}`}>
+                <div className="flex relative">
+                  <img
+                    alt="gallery"
+                    className="absolute inset-0 w-full h-full object-cover object-center"
+                    src={recipe.picture}
+                  />
+                  <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                      {recipe.category}
+                    </h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                      {recipe.name}
+                    </h1>
+                    <p className="leading-relaxed">{recipe.recipe}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
