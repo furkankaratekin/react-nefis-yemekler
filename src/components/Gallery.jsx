35af100b7a5ed6594c4e081eb4cab54d4ef1283d
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-31 12:48:24 +0300

    Ana Sayfa tarifler geldi

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Gallery = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://yemek-api-vercel.onrender.com/api/recipe")
+      .then((response) => {
+        setRecipes(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the recipes!", error);
+      });
+  }, []);
+
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <Link to="/list-recipe">
-          <div class="flex flex-wrap -m-4">
-            <div class="lg:w-1/4 sm:w-1/2 p-4">
-              <div class="flex relative">
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {recipes.map((recipe) => (
+            <div className="lg:w-1/4 sm:w-1/2 p-4" key={recipe._id}>
+              <div className="flex relative">
                 <img
                   alt="gallery"
-                  class="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/600x360"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={recipe.picture}
                 />
-                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    KATEGORİ
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    {recipe.category}
                   </h2>
-                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                    Yemeğin Adı
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    {recipe.name}
                   </h1>
-                  <p class="leading-relaxed">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+                  <p className="leading-relaxed">{recipe.recipe}</p>
                 </div>
               </div>
             </div>
-          </div>
-        </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
